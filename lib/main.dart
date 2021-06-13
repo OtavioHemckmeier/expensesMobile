7d35e731cdd870a3d87da81b11cc60663b27a500
Otavio <otavio@conectra.net.br>
2021-06-13 17:42:19 -0300

    [ADD] Adicionado mensagem informando que não existe nenhuma transação cadastrada.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:expenses/components/transaction_form.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-
+import 'dart:math';
+import './components/transaction_list.dart';
+import './components/transaction_form.dart';
 import 'Tansaction.dart';
 
 main() => runApp(ExpensesApp());
@@ -10,88 +12,82 @@ class ExpensesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: MyHomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+        accentColor: Colors.amber,
+      ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final _transaction = [
-    Transaction(
-        id: 't1', title: 'Novo tenis', value: 310.55, date: DateTime.now()),
-    Transaction(
-        id: 't2', title: 'Novo bola', value: 310.55, date: DateTime.now()),
-    Transaction(
-        id: 't3', title: 'Novo camisa', value: 310.55, date: DateTime.now()),
-    Transaction(
-        id: 't4', title: 'Novo sapato', value: 310.55, date: DateTime.now()),
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _transaction = [
+    // Transaction(
+    //     id: 't1', title: 'Novo tenis', value: 310.55, date: DateTime.now()),
+    // Transaction(
+    //     id: 't1', title: 'Novo tenis', value: 310.55, date: DateTime.now()),
+    // Transaction(
+    //     id: 't1', title: 'Novo tenis', value: 310.55, date: DateTime.now()),
   ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble.toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transaction.add(newTransaction);
+    });
+
+    Navigator.of(context).pop();
+  }
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return TransactionForm(_addTransaction);
+        }
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas pessoais'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            width: double.infinity,
-            child: Card(
-              color: Colors.blue,
-              child: Text('Gráfico'),
-              elevation: 5,
-            ),
-          ),
-          Column(
-            children: _transaction.map((tr) {
-              return Card(
-                child: Row(
-                  children: [
-                    Container(
-                      margin:
-                      EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.purple,
-                            width: 2,
-                          )),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        'R\$ ${tr.value}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          tr.title,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          DateFormat('d MMM y').format(tr.date),
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              );
-            }).toList(),
-          )
+        actions: [
+          IconButton(icon: Icon(Icons.add_circle_outlined), onPressed: () => _openTransactionFormModal(context))
         ],
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('Gráfico'),
+                elevation: 5,
+              ),
+            ),
+            TransactionList(_transaction),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: IconButton(icon: Icon(Icons.add_circle_outlined), onPressed: () => _openTransactionFormModal(context))
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
